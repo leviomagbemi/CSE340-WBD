c6c0ce3f2f03fba48f86c6443ba7390940bd778c
Levi Omagbemi <leviomagbemi15@gmail.com>
2026-01-19 23:05:44 +0100

    assignment 2 complete your database

---
+++ database/assignment2.sql
@@ -0,0 +1,49 @@
+-- Data for table `Account`
+INSERT INTO public.account (
+  account_firstname,
+  account_lastname,
+  account_email,
+  account_password
+) VALUES (
+  'Tony',
+  'Stark',
+  'tony@starkent.com',
+  'Iam1ronM@n'
+);
+
+-- Update account type
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+-- Delete from table
+DELETE FROM public.account
+WHERE account_id = 1;
+
+-- Update inventory row description based on model and make
+UPDATE public.inventory
+SET inv_description = REPLACE(
+    inv_description, 
+    'small interiors', 
+    'a huge interior'
+)
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- Select the make and model fields from the inventory table and the classification name for inventory items that belongs to `sport`
+SELECT 
+    i.inv_make, 
+    i.inv_model, 
+    c.classification_name
+FROM 
+    public.inventory i
+INNER JOIN 
+    public.classification c 
+    ON i.classification_id = c.classification_id
+WHERE 
+    c.classification_name = 'Sport';
+
+-- Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query.
+UPDATE public.inventory
+SET 
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
